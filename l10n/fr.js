ab2c962ce1dd117455190372b35bf37618d50522
Marius David Wieschollek <git.public@mdns.eu>
2024-04-27 21:36:51 +0200

    Added french

---
+++ l10n/fr.js
@@ -0,0 +1,11 @@
+(function() {
+    OC.L10N.register("htmlviewer", {
+        'Warning'                                                                          : 'Alerte',
+        'Cancel'                                                                           : 'Annuler',
+        'Ok'                                                                               : 'Ok',
+        'View'                                                                             : 'Voir',
+        'Do not warn again'                                                                : 'Ne pas réitérer l\'alerte',
+        'Opening HTML files in the browser can be a security risk. Do you want to proceed?': 'Ouvrir des fichiers HTML dans le navigateur peut être un risque pour la sécurité. Vous souhaitez continuer ?',
+        'The file "{file}" ({size}) exceeds the maximum allowed size of {maxSize}.'        : 'Le fichier « {file} » ({size}) dépasse la taille de fichier autorisée de {maxSize}.'
+    }, "nplurals=2; plural=(n != 1);");
+}());
